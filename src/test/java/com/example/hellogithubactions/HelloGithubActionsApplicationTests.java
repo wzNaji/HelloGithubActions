198abc4d86ac1b865bfce1ca59615fdbf05a8961
wzNaji <141943564+wzNaji@users.noreply.github.com>
2024-02-29 17:42:26 +0100

    Initial commit

---
+++ src/test/java/com/example/hellogithubactions/HelloGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
